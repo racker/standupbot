9e1407c8a614cb5614a9e7abda2e07bfab086d07
James Burkhart <james@jamesburkhart.com>
2012-12-06 14:42:08 -0800

    refactor irc related code out into a separate file

--- standupbot.js
+++ standupbot.js
@@ -11,13 +11,13 @@
 
 // Imports
 var express = require('express');
-var irc = require('irc');
 var fs = require('fs');
 var yaml = require('js-yaml');
 var async = require('async');
 var cron = require('cron').CronJob;
 var jade = require('jade');
 var sqlite = require('sqlite3');
+var ircHandler = require('./ircHandler');
 
 // Open db and make sure stats table exists
 var db = new sqlite.Database('stats.db');
@@ -37,66 +37,11 @@ if (process.argv.length > 2) {
 var contents = fs.readFileSync(configFile).toString();
 var config = yaml.load(contents).config;
 
+ircHandler.init(config);
 
-// Load Configuration Variables
-var nick = config.irc.nick;
-var user_name = config.irc.user_name;
-var real_name = config.irc.real_name;
-var server = config.irc.server;
-var port = config.irc.port;
-var ssl = config.irc.ssl;
-var channels_def = [];
-{
-  a = config.irc.channels.definitions;
-  for (var i = 0; i < a.length; i++) {
-    if ('password' in a[i]) {
-        channels_def.push(a[i].name + " " + a[i].password);
-    }
-    else {
-        channels_def.push(a[i].name);
-    }
-  }
-}
-var channels_publish = config.irc.channels.publish;
-var channels_remind  = config.irc.channels.remind;
-var members = config.members;
-var members_dir = config.members_dir;
-
-if (!fs.existsSync(members_dir)) {
-  fs.mkdirSync(members_dir);
-  console.log('created members dir at: ' + members_dir);
-}
-var domain = config.domain;
 
 // Timers
 var timers = config.timers
-
-// Connect to IRC
-var irc = new irc.Client(server, nick,
-       {
-         userName: user_name,
-         realName: real_name,
-         debug: true,
-         showErrors: true,
-         port: port,
-         autoRejoin: true,
-         autoConnect: false,
-         channels: channels_def,
-         secure: ssl,
-         selfSigned: true,
-         certExpired: true,
-         floodProtection: true,
-         floodProtectionDelay: 250,
-         stripColors: false
-       });
-
-// Connect IRC client and initize timers
-//irc.connect(function() {
-  //new cron(timers.earlyReminder, announceEarlyReminder, null, true);
-  //new cron(timers.dueReminder, announceDueReminder, null, true);
-  //new cron(timers.lateReminder, announceLateReminder, null, true);
-  //new cron(timers.deadlineReminder, announceDeadlineReminder, null, true);
-//});
 
 // Initiate the web framework
 var app = express();
@@ -133,39 +78,17 @@ app.post('/irc', function(req, res){
   result += "---------------------------------------\n"
 
   console.log(result);
-  res.cookie('irc_nick', req.body.irc_nick, { domain: domain });
-  res.cookie('area', req.body.area, { domain: domain });
+  res.cookie('irc_nick', req.body.irc_nick, { domain: config.domain });
+  res.cookie('area', req.body.area, { domain: config.domain });
   res.send("<pre>\n" + result + "\n</pre>");
 
-  publishToChannels(result, function () {
-    fs.writeFile(members_dir + "/" + req.body.irc_nick, result, function(err) {
+  ircHandler.publishToChannels(result, function () {
+    fs.writeFile(config.members_dir + "/" + req.body.irc_nick, result, function(err) {
       console.log("Logged " + req.body.irc_nick + "'s standup.");
     });
   });
   saveRow(req.body.irc_nick, finished, inProgress, impediments); 
 });
-
-function publishToChannels(message, callback) {
-  var publish = function publish(channel, callback) {
-    irc.say(channel, message);
-    callback();
-  };
-
-  async.forEach(channels_publish, publish, function(err) {
-    callback();
-  });
-}
-
-function remindChannels(message, callback) {
-  var remind = function remind(channel, callback) {
-    irc.notice(channel, message);
-    callback();
-  };
-
-  async.forEach(channels_remind, remind, function(err) {
-    callback();
-  });
-}
 
 function saveRow(name, finished, inProgress, impediments, callback) {
   var stmt,
@@ -200,7 +123,7 @@ function label_and_break_lines(label, msg) {
 function checkForMissingStandups(callback) {
   var missing = [];
   console.log("checking for missing standups");
-  fs.readdir(members_dir, function(err, contents) {
+  fs.readdir(config.members_dir, function(err, contents) {
     for (var i=0; i < members.length; i++) {
       if (contents.indexOf(members[i]) == -1) {
         missing.push(members[i]);
@@ -210,59 +133,18 @@ function checkForMissingStandups(callback) {
   });
 }
 
-function clearMemberStandups(callback) {
-  fs.readdir(members_dir, function(err, contents) {
-    for (var i=0; i < contents.length; i++) {
-      fs.unlink(members_dir + '/' + contents[i]);
+function getHistoricalData() {
+  var rows = [];
+  db.each("SELECT * FROM stats", function(err, row) {
+    if (err) {
+      console.log("Encountered an error reading from database!");
+      console.log(err)
+    } else {
+      rows.append(row);
     }
-  });
-  callback();
-}
-
-function announceEarlyReminder() {
-  checkForMissingStandups(function(err, missing) {
-    var msg = 'Standups are due soon. (' + missing.join(', ') + ')';
-    remindChannels(msg, function() {
-      console.log('Reminded channel that standups are due soon.');
-    });
+    return {rows: rows};
   });
 }
-
-function announceDueReminder() {
-  checkForMissingStandups(function(err, missing) {
-    var msg = 'Standups are due! (' + missing.join(', ') + ')';
-    remindChannels(msg, function() {
-      console.log('Reminded channel that standups are due now.');
-    });
-  });
-}
-
-function announceLateReminder() {
-  checkForMissingStandups(function(err, missing) {
-    var msg = 'Standups are late! (' + missing.join(', ') + ')';
-    remindChannels(msg, function() {
-      console.log('Reminded channels that standups are late.');
-    });
-  });
-}
-
-function announceDeadlineReminder() {
-  checkForMissingStandups(function(err, missing) {
-    var msg = 'The deadline for standups is now. You lose the game! (' + missing.join(', ') + ')';
-    remindChannels(msg, function() {
-      console.log('Reminded channels that the deadline for standups has passed.');
-      clearMemberStandups(function() {
-        console.log('Cleared member standups.');
-      });
-    });
-  });
-}
-
-
-// Add listener for error events so the bot doesn't crash when something goes wrong on the server
-irc.addListener('error', function(message) {
-  console.log('error: ', message);
-});
 
 process.on('SIGINT', function() {
   console.log("\nGracefully shutting down from SIGINT (Ctrl+C)");
